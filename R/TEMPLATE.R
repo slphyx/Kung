9b6dc5c0b3be2c8af8fa0110dd3cffb9849c68a8
Sompob Saralamba <sakngoi@hotmail.com>
2019-05-06 00:02:46 +0700

    first commit

---
+++ R/TEMPLATE.R
@@ -0,0 +1,105 @@
+ui <- '
+
+# Sompob Saralamba <saralamba@gmail.com>
+
+renderInputs <- function(){
+
+
+  fluidRow(
+    column(6,
+      CONTROLS
+    )
+  )
+
+
+}
+
+
+
+fluidPage(
+# using css theme;
+# theme="simplex.min.css",
+#   tags$style(type="text/css",
+#   "label {font-size: 12px;}",
+#   ".recalculating {opacity: 1.0;}"
+#   ),
+
+  # title
+  tags$h2("Mahidol Oxford Tropical Medicine Research Unit"),
+  hr(),
+
+  fluidRow(
+    column(6, tags$h3("System Parameters"))
+  ),
+  # add the controls
+  fluidRow(
+    column(6, renderInputs())
+  ),
+
+  #
+  fluidRow(
+    column(1,NULL),
+    column(10,plotOutput("graphs")),
+    column(1,NULL)
+  )
+
+)
+'
+
+
+sv <- '
+
+# Sompob Saralamba <saralamba@gmail.com>
+
+library(deSolve)
+
+ODEFUNC
+
+RunOde <- function(parms){
+
+  PARAMETERS
+
+  ODECMD
+
+return(out)
+}
+
+
+shinyServer(
+
+function(input, output, session) {
+
+
+  parms <- reactive(c(
+
+    INPUTCONTROLS
+
+  ))
+
+
+  outode <- reactive(RunOde(parms()))
+
+
+  #plotting function
+  plotX <- function(){
+
+    out <- outode()
+
+    POSTPROCESS
+
+    PLOTCODES
+
+  }
+
+
+  output$graphs <- renderPlot({
+    plotX()
+  })
+
+})
+
+
+
+
+
+'
---
+++ R/runapps.R
@@ -0,0 +1,43 @@
+
+# run the shinyapp
+runSystem <- function(systemfile, ...){
+
+
+  # message(paste0("tempory directory has been created...",tmpdir))
+  #
+  # #write ui/server files
+  # uifile <- paste0(tmpdir,"/ui.R")
+  # svfile <- paste0(tmpdir,"/server.R")
+  #
+  # WriteTxt(txt = ui, filename=uifile)
+  # message(paste0(uifile," has been written."))
+  # WriteTxt(txt = sv, filename=svfile)
+  # message(paste0(svfile," has been written."))
+  txt <- ReadInputString(filename = systemfile)
+  tmpdir <- tempdir()
+
+  GenShinyFiles(txt = txt, path = tmpdir)
+
+  #launch the app
+  message("\nLaunching Shiny interface... ",
+          "for large models this  may take some time.")
+
+  shiny::runApp(tmpdir,...)
+
+}
+
+
+## export the shiny app files; ui.R and server.R
+saveSystem <- function(txt,path){
+
+
+  #write ui/server files
+  uifile <- paste0(path,"/ui.R")
+  svfile <- paste0(path,"/server.R")
+
+  WriteTxt(txt = ui, filename=uifile)
+  message(paste0(uifile," has been written."))
+  WriteTxt(txt = sv, filename=svfile)
+  message(paste0(svfile," has been written."))
+
+}
